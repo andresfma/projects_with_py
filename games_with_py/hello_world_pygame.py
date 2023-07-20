e82763e535b7e4891bfdde0c5cb1da904519feb5
Andres Mercado <116472462+andresfma@users.noreply.github.com>
2023-07-20 16:05:34 -0500

    adding animation.py file

--- games_with_py/hello_world_pygame.py
+++ games_with_py/hello_world_pygame.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 pygame.init()
 
 #set up the window
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+windowSurface = pygame.display.set_mode((800, 400), 0, 32)
 pygame.display.set_caption('Hello world!')
 
 #set up the colors
@@ -24,7 +24,7 @@ textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
 
-#draw thE white background onto the surface
+#draw the white background onto the surface
 windowSurface.fill(WHITE)
 
 #draw a green polygon onto the surface
@@ -32,14 +32,14 @@ pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56
 
 #draw some blue lines onto the surfaces
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
+pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120), 10)
 pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
 
 #draw a blue circle onto the surface
-pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 40, 4)
 
 #draw a red ellipse onto the surface
-pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 0)
 
 #draw the text's background rectangle onto the surface
 pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
